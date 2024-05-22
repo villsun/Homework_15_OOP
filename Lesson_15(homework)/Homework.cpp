9973e7d1a9e4ed38cf0b284560c0c46ace53eb1c
villsun <lonukkola@gmail.com>
2024-05-22 20:22:58 +0200

    Add functions Delete_Element and Show_Reverse

--- Lesson_15(homework)/Homework.cpp
+++ Lesson_15(homework)/Homework.cpp
@@ -1,10 +1,54 @@
 #include <iostream>
+#include <list>
+#include <stdexcept>
 
 using namespace std;
 
+template <typename T>
+void Delete_Element(list<T>& obj)
+{
+	int index;
+	cout << "Enter index which you want to delete:\n";
+	cin >> index;
+	if (obj.empty() || index<0 || index>obj.size())
+	{
+		throw runtime_error("Error!");
+	}
+	auto it = obj.begin();
+	advance(it, index);
+	obj.erase(it);
+}
+
+template <typename T>
+void Show_Reverse(list<T>& obj)
+{
+	if (obj.empty())
+	{
+		throw runtime_error("Error!");
+	}
+
+	auto it = obj.end();
+	do
+	{
+		it--;
+		cout << *it << ' ';
+	} while (it!=obj.begin());
+	cout << endl;
+}
+
 int main()
 {
-
+	list<int> obj;
+	try
+	{
+		Show_Reverse(obj);
+		Delete_Element(obj);
+		Show_Reverse(obj);
+	}
+	catch (const runtime_error& e)
+	{
+		cout << e.what() << endl;
+	}
 
 	return 0;
 }
